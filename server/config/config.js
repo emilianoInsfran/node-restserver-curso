a5de680b552b0042a8503050856693e595a9fb32
Emiliano <emilianoinsfran@gmail.com>
2020-05-10 05:56:38 -0300

    variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ let urlDB ;
 if(process.env.NODE_ENV == 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb://cafe-user:emi123@ds119091.mlab.com:19091/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB
